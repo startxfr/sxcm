cc1ba472d32822a3147c8cc176ee281fa5034917
Christophe <clarue@startx.fr>
2021-05-27 09:31:35 +0200

    [cli] Debug applying argoCD applications

--- src/resources/argocd-project/upsert.sh
+++ src/resources/argocd-project/upsert.sh
@@ -5,6 +5,7 @@ ENV=$3
 VERSION=$4
 OCTOKEN=$5
 OCCLUSTER=$6
+OCCLUSTERNAME=$7
 ARGOCDPREFIX=startx
 PROJECT=startx-argocd
 DIR=`dirname $(readlink -f $0)`
@@ -12,11 +13,7 @@ DIR="$DIR/_console/okd-cluster-resources/argod-project"
 SERVER_NAME="argocd-operator"
 DELAY="60"
 
-echo "STOP WORD IN ARGOCD PROJECT DEPLOY !!!!!!!!!!!"
-exit
-
-
-oc wait po -l name=$SERVER_NAME --for=condition=Ready --timeout=500ms -n $PROJECT --token=$OCTOKEN --cluster=$OCCLUSTER &>/dev/null
+oc wait po -l name=$SERVER_NAME --for=condition=Ready --timeout=500ms -n $PROJECT --token=$OCTOKEN --cluster=$OCCLUSTERNAME &>/dev/null
 if [[ $? == "0" ]]
 then
     echo -e "Operator $SERVER_NAME in $PROJECT is started"
@@ -24,16 +21,16 @@ else
     echo -e "Operator $SERVER_NAME in $PROJECT is not present, probably under deployment. Wait ${DELAY}s..."
     echo -e "${DELAY}sec left..."
     sleep ${DELAY}
-    oc wait po -l name=$SERVER_NAME --for=condition=Ready --timeout=${DELAY}0ms -n $PROJECT --token=$OCTOKEN --cluster=$OCCLUSTER &>/dev/null
+    oc wait po -l name=$SERVER_NAME --for=condition=Ready --timeout=${DELAY}0ms -n $PROJECT --token=$OCTOKEN --cluster=$OCCLUSTERNAME &>/dev/null
     if [[ $? == "0" ]]
     then
         echo -e "Operator $SERVER_NAME in $PROJECT is now started"
     else
-        oc get po -n $PROJECT --token=$OCTOKEN --cluster=$OCCLUSTER
+        oc get po -n $PROJECT --token=$OCTOKEN --cluster=$OCCLUSTERNAME
         echo -e "Operator $SERVER_NAME in $PROJECT is still not ready. Wait ${DELAY}s more..."
         echo -e "$(echo ${DELAY}/2 | bc)sec left..."
         sleep $(echo ${DELAY}/2 | bc)
-        oc wait po -l name=$SERVER_NAME --for=condition=Ready --timeout=${DELAY}0ms -n $PROJECT --token=$OCTOKEN --cluster=$OCCLUSTER &>/dev/null
+        oc wait po -l name=$SERVER_NAME --for=condition=Ready --timeout=${DELAY}0ms -n $PROJECT --token=$OCTOKEN --cluster=$OCCLUSTERNAME &>/dev/null
         if [[ $? == "0" ]]
         then
             echo -e "Operator $SERVER_NAME in $PROJECT is started"
@@ -43,5 +40,5 @@ else
         fi
     fi
     echo -e "Add anyuid SCC to argocd-redis-ha service account in $PROJECT namespace"
-    oc adm policy add-scc-to-user anyuid -z argocd-redis-ha -n $PROJECT --token=$OCTOKEN --cluster=$OCCLUSTER &>/dev/null
+    oc adm policy add-scc-to-user anyuid -z argocd-redis-ha -n $PROJECT --token=$OCTOKEN --cluster=$OCCLUSTERNAME &>/dev/null
 fi
